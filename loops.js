ff45c389b911ad95a83d496b378fdc886cf3e34c
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-27 02:17:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,12 +17,13 @@ function whileLoop(n){
   return('done')
 }
 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
+
 
 function doWhileLoop(num){
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
   do{
     console.log("I run once regardless.");
   }
